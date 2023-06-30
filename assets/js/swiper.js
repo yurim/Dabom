f4448f3d342ffd6869226df86d914c21845eb2ee
yurim <nogod829@gmail.com>
2023-06-30 20:31:51 +0900

    메인 페이지 색션3 완료

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,49 +1,20 @@
-
 const recordSlide = new Swiper(".record_slide", {
-    direction: "horizontal",
-    slidesPerGroup : 1,
-    slidesPerView: "3",
-    spaceBetween: 20,
-    loop: false,
-    pagination: {
-        el: ".swiper-pagination",
-        type: "fraction",
-    },
-    lazy : {
-        loadPrevNext : true
+    direction: "horizontal", slidesPerGroup: 1, slidesPerView: "3", spaceBetween: 20, loop: false, pagination: {
+        el: ".swiper-pagination", type: "fraction",
+    }, lazy: {
+        loadPrevNext: true
     },
 });
 
 
-// const storySlide = new Swiper(".story_slide", {
-//     direction: "horizontal",
-//     slidesPerView: 'auto',
-//     spaceBetween: 10,
-//     loop: true,
-//     slidesOffsetAfter: '10',
-//     loopAdditionalSlides: 1,
-//     navigation: {
-//         nextEl: ".swiper-button-next",
-//         prevEl: ".swiper-button-prev",
-//     },
-//     lazy : {
-//         loadPrevNext : true
-//     },
-// });
-
-    const storySlide = new Swiper(".story_slide", {
-            slidesPerView: 'auto',
-        // spaceBetween: 10,
-        rewind: true,
-        loop: true,
-        loopAdditionalSlides: 1,
-        navigation: {
-            nextEl: ".swiper-button-next",
-            prevEl: ".swiper-button-prev",
-        },
-        lazy : {
-            loadPrevNext : true
-        },
-
-    });
+const storySlide = new Swiper(".story_slide", {
+    slidesPerView: 'auto',
+    rewind: true,
+    loopAdditionalSlides: 1,
+    navigation: {
+        nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev",
+    }, lazy: {
+        loadPrevNext: true
+    },
+});
 
