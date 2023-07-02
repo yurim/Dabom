e5da000c793c89178c5c94aeed452c8065993207
yurim <nogod829@gmail.com>
2023-07-02 19:21:12 +0900

    메인 페이지 색션5 완료

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,5 +1,10 @@
 const recordSlide = new Swiper(".record_slide", {
-    direction: "horizontal", slidesPerGroup: 1, slidesPerView: "3", spaceBetween: 20, loop: false, pagination: {
+    direction: "horizontal",
+    slidesPerGroup: 1,
+    slidesPerView: "3",
+    spaceBetween: 20,
+    loop: false,
+    pagination: {
         el: ".swiper-pagination", type: "fraction",
     }, lazy: {
         loadPrevNext: true
@@ -11,6 +16,7 @@ const storySlide = new Swiper(".story_slide", {
     slidesPerView: 'auto',
     rewind: true,
     loopAdditionalSlides: 1,
+    speed: 500,
     navigation: {
         nextEl: ".swiper-button-next", prevEl: ".swiper-button-prev",
     }, lazy: {
